705d6e487aa084628cbf157390f8b0d037985cfa
Jahirul83 <jahirulislamcse83@gmail.com>
2025-07-13 15:19:05 +0600

    delete button functional

--- src/components/DynamicForm/DynamicForm.jsx
+++ src/components/DynamicForm/DynamicForm.jsx
@@ -6,28 +6,28 @@ const DynamicForm = () => {
     const handleAdd = () => {
         setFields([...fields, { input: '', select: '' }]);
     };
+    const handleDelete = (index) =>{
+         setFields(fields.filter((item,idx) => idx!== index));
+    }
     return (
         <div className='flex items-center justify-center'>
             <div>
                 {fields.map((field, index) => (
                     <div key={index} className="flex gap-4 mb-2">
                         <input
-                            placeholder='name'
                             className='input'
                             type="text"
-                            value={field.input}
 
                         />
 
                         <select className="select"
-                            value={field.select}
 
                         >
                             <option value="">Select</option>
                             <option value="Option1">Option 1</option>
                             <option value="Option2">Option 2</option>
                         </select>
-                        <button className='btn btn-error text-white'>Delete</button>
+                        <button onClick={()=>handleDelete(index)} className='btn btn-error text-white'>Delete</button>
                     </div>
                 ))}
                 <button className='btn' onClick={handleAdd}>+ Add more</button>
